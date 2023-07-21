d51430f91ef5b388ca7e61a414153fc4a140855e
Jaymineh <91850543+jaymineh@users.noreply.github.com>
2023-07-21 17:45:56 +0100

    deleted keyID

--- modules/EFS/efs.tf
+++ modules/EFS/efs.tf
@@ -4,7 +4,6 @@ resource "aws_kms_key" "ACS-kms" {
   policy      = <<EOF
   {
   "Version": "2012-10-17",
-  "Id": "kms-key-policy",
   "Statement": [
     {
       "Sid": "Enable IAM User Permissions",
